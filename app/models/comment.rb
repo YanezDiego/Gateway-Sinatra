696a3acd50a2f20273b41dfff2747a0958309a3b
Diego Yanez <diegoyanez@MacBook-Pro-4.local>
2018-06-14 14:42:45 -0400

    added validate_presence of ussername,email,content(question and comment), has secure password

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
+
+  validates_presence_of :content
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :questions
   has_many :comments
+
+  validates_presence_of :ussername, :email
+
+  has_secure_password
 end
