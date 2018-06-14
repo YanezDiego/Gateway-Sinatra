3b409c180947aa264642739f72b9cb81110670bd
Diego Yanez <diegoyanez@MacBook-Pro-4.local>
2018-06-14 16:42:54 -0400

    added login route and logic for user. plus profile page

--- app/controllers/user_application_controller.rb
+++ app/controllers/user_application_controller.rb
@@ -1,4 +1,31 @@
 class UserApplicationController < ApplicationController
 
+  get '/user/login' do
+    if !logged_in?
+      erb :'/user/login'
+    else
+      redirect to 'user/profile'
+    end
+  end
+
+  post '/user/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.ide
+      redirect to '/user/profile'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/user/profile' do
+    if logged_in?
+      erb :'/user/profile'
+    else
+      refirect to '/user/login'
+    end
+  end
+
+
 
 end
