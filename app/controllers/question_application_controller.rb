2db0f62d2bc3f125c5f8351883603277c282b76f
Diego Yanez <diegoyanez@MacBook-Pro-4.local>
2018-06-15 12:43:15 -0400

    added index route plus post /question & question.id show'

--- app/controllers/question_application_controller.rb
+++ app/controllers/question_application_controller.rb
@@ -2,7 +2,8 @@ class QuestionApplicationController < ApplicationController
 
   get '/questions' do
     if logged_in?
-     erb :'/questions/show'
+      @questions = Question.all
+     erb :'/questions/index'
     else
       redirect to '/'
     end
@@ -15,5 +16,28 @@ class QuestionApplicationController < ApplicationController
       redirect to '/'
     end
   end
+
+  post '/questions' do
+    if logged_in?
+      @question = current_user.questions.build(content: params[:content])
+      if @question.save
+        redirect to "/questions/#{@question.id}"
+      else
+        redirect to '/questions/new'
+      end
+    else
+      redirect to '/'
+    end
+  end
+
+  get '/questions/:id' do
+    if logged_in?
+      @question = Question.find_by_id(params[:id])
+      erb :'/questions/show'
+    else
+      redirect to '/'
+    end
+  end
+
 
 end
