e8ec1ab3a086269fe66fedc983460c1b5b92d5bc
Diego Yanez <diegoyanez@MacBook-Pro-4.local>
2018-06-15 11:56:24 -0400

    added a show route, and new route to question control.

--- app/controllers/question_application_controller.rb
+++ app/controllers/question_application_controller.rb
@@ -1,4 +1,19 @@
 class QuestionApplicationController < ApplicationController
 
+  get '/questions' do
+    if logged_in?
+     erb :'/questions/show'
+    else
+      redirect to '/'
+    end
+  end
+
+  get '/questions/new' do
+    if logged_in?
+      erb :'/questions/new'
+    else
+      redirect to '/'
+    end
+  end
 
 end
