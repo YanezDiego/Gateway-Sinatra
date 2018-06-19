41fdd802046ca4dac6bd1c5c568d6fced51820b5
Diego Yanez <diegoyanez@MacBook-Pro-4.fios-router.home>
2018-06-19 12:11:54 -0400

    verifys uniqness of username, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   has_many :comments
 
   validates_presence_of :username, :email
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
 
   has_secure_password
 end
